c44c68c7cf64107f585049a1989b6f7c16614384
Brandon Boi <brandonboi@my.uopeople.edu>
2026-03-19 12:31:59 +0300

    onboarding update

--- api/src/schools/dto/create-school.dto.ts
+++ api/src/schools/dto/create-school.dto.ts
@@ -1,5 +1,73 @@
-import { IsEnum, IsOptional, IsString, MinLength } from "class-validator";
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsObject,
+  IsOptional,
+  IsString,
+  MinLength,
+  ValidateNested,
+} from "class-validator";
+import { Type } from "class-transformer";
 import { InstitutionType } from "@prisma/client";
+
+class AcademicSetupDto {
+  @IsOptional()
+  @IsString()
+  label?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  selectedItems?: string[];
+
+  @IsOptional()
+  @IsBoolean()
+  setUpLater?: boolean;
+}
+
+class InstitutionProfileDto {
+  @IsOptional()
+  @IsString()
+  learningMode?: string;
+
+  @IsOptional()
+  @IsString()
+  ownership?: string;
+
+  @IsOptional()
+  @IsString()
+  levelType?: string;
+}
+
+class PhoneDto {
+  @IsOptional()
+  @IsString()
+  countryCode?: string;
+
+  @IsOptional()
+  @IsString()
+  dialCode?: string;
+
+  @IsOptional()
+  @IsString()
+  nationalNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  e164?: string;
+}
+
+class SecurityDto {
+  @IsOptional()
+  @IsBoolean()
+  addPhoneLater?: boolean;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => PhoneDto)
+  phone?: PhoneDto | null;
+}
 
 export class CreateSchoolDto {
   @IsString()
@@ -12,11 +80,20 @@ export class CreateSchoolDto {
 
   @IsOptional()
   @IsString()
+  countryCode?: string;
+
+  @IsOptional()
+  @IsString()
   curriculumName?: string;
 
   @IsOptional()
   @IsString()
   curriculumCode?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  curricula?: string[];
 
   @IsOptional()
   @IsString()
@@ -29,4 +106,19 @@ export class CreateSchoolDto {
   @IsOptional()
   @IsEnum(InstitutionType)
   institutionType?: InstitutionType;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => AcademicSetupDto)
+  academicSetup?: AcademicSetupDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => InstitutionProfileDto)
+  institutionProfile?: InstitutionProfileDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => SecurityDto)
+  security?: SecurityDto;
 }
